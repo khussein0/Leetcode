f0a35939d3f1ca7eb73e44aa5f4b44a8a8fdbe40
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-07-02 12:30:29 +0530

    Time: 219 ms (17.91%), Space: 51 MB (68.66%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
@@ -11,13 +11,12 @@ var maxArea = function(h, w, horizontalCuts, verticalCuts) {
     let maxH = Math.max(horizontalCuts[0], (h - horizontalCuts[horizontalCuts.length - 1])),
         maxV = Math.max(verticalCuts[0], (w - verticalCuts[verticalCuts.length - 1]));
     
-    for(var i = 1; i < Math.max(verticalCuts.length, horizontalCuts.length); i++){
-        if(verticalCuts[i] && verticalCuts.length > 1){
-            maxV = Math.max(maxV, (verticalCuts[i] - verticalCuts[i - 1]));
-        }
-        if(horizontalCuts[i] && horizontalCuts.length > 1){
-            maxH = Math.max(maxH, (horizontalCuts[i] - horizontalCuts[i - 1]))
-        }
+    for(let i = 1; i < horizontalCuts.length; i++){
+        maxH = Math.max(maxH, (horizontalCuts[i] - horizontalCuts[i - 1]));
+    }
+    
+    for(let i = 1; i < verticalCuts.length; i++){
+        maxV = Math.max(maxV, (verticalCuts[i] - verticalCuts[i - 1]));
     }
     
     return BigInt(maxH) * BigInt(maxV) % 1000000007n
