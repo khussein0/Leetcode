3a0ff07b677d91898f651d7e7cddaf40259e8579
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-07-07 13:27:53 +0530

    Time: 83 ms (81.74%), Space: 42.6 MB (98.40%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.js
+++ 97-interleaving-string/97-interleaving-string.js
@@ -11,8 +11,7 @@ var isInterleave = function(s1, s2, s3) {
     dp[1] = 1;
     for (let i = 1; i < n; i++){
         for (let j = 1; j < m; j++) {
-            let up = dp[j] && s1[i-2] === s3[j+i-3], left = dp[j-1] && s2[j-2] === s3[j+i-3];
-            dp[j] = up || left;
+            dp[j] = dp[j] && s1[i-2] === s3[j+i-3] || dp[j-1] && s2[j-2] === s3[j+i-3];
         }
     }
     return dp[m-1];
