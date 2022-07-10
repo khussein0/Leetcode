6406664d315b4d4b445ce0436e0ed36371ee66ae
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-07-10 13:17:22 +0530

    Time: 76 ms (81.67%), Space: 42.6 MB (76.95%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    for(var i = cost.length - 3; i >= 0; i--) cost[i] += Math.min(cost[i+1], cost[i+2]);
+    return Math.min(cost[0], cost[1]);
+};
