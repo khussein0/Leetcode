b478bd78fed869b858179a7d1497ce9c06773928
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-07-25 13:34:09 +0530

    Time: 126 ms (5.16%), Space: 42.9 MB (27.56%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    function bSearch(nums, target, left = 0, right = nums.length - 1){
+        while(left <= right){
+            let mid = Math.floor(left + (right - left) / 2);
+            if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+    
+    let l = bSearch(nums, target);
+    if(nums[l] !== target) return [-1, -1];
+    return [l, bSearch(nums, target + 1, l) - 1];
+};
