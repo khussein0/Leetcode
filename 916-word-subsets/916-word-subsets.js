22ee65cee09493a2b972e3ac0706e0080f056086
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-07-30 20:55:59 +0530

    Time: 355 ms (38.46%), Space: 66.1 MB (30.77%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {string[]}
+ */
+var wordSubsets = function(words1, words2) {
+    const output = [];
+    const freqB = Array(26).fill(0);
+    
+    for(let wordB of words2) {
+        const currFreq = getFreq(wordB);
+        
+        for(let i = 0; i < freqB.length; i++) {
+            freqB[i] = Math.max(freqB[i], currFreq[i]);
+        };
+    };
+    
+    for(let wordA of words1) {
+        const freqA = getFreq(wordA)
+        const universal = isSubset(freqA);
+        if(universal) output.push(wordA);
+    };
+    
+    function isSubset(freqA) {
+        for(let i = 0; i < freqA.length; i++) {
+            if(freqB[i] > freqA[i]) return false;
+        };
+        return true;
+    };
+    
+    function getFreq(word) {
+        const map = Array(26).fill(0);
+        
+        for(let char of word) {
+            const idx = char.charCodeAt() - 'a'.charCodeAt();
+            map[idx]++;
+        };
+        return map;
+    };
+    return output;
+};
