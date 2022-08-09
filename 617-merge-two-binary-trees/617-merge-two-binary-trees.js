fc375476a36fecb17ddba627bb6ee8c4610df543
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-08-09 16:11:47 +0530

    Time: 141 ms (64.84%), Space: 49.9 MB (82.19%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(root1, root2) {
+    if(!root1 || !root2) return root1 || root2;
+    return new TreeNode(root1.val + root2.val, mergeTrees(root1.left, root2.left), mergeTrees(root1.right, root2.right));
+};
