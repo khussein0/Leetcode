7978217421fe721938ec96878eaba43fa6f35271
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-08-25 08:52:51 +0530

    Time: 143 ms (38.84%), Space: 44.8 MB (61.72%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -4,15 +4,15 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    const arr = ransomNote.split('');
-    for (const char of magazine) {
-        const index = arr.indexOf(char);
-        if (index > -1) {
-          arr.splice(index, 1);
-        };
-        if (!arr.length) {
-            return true;
-        };
+    let map = {};
+    for(let char of magazine){
+        map[char] = (map[char] || 0) + 1;
     };
-    return false;
+    
+    for(let char of ransomNote){
+      if(!map[char]) return false;
+      map[char]--;  
+    };
+    
+    return true;
 };
