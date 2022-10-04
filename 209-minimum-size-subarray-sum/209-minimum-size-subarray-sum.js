c475d31a5004d35893f8d3a5781d8a80af79ab08
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-10-04 19:12:46 +0530

    Time: 110 ms (46.69%), Space: 46.3 MB (36.42%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let r = 0, l = 0, sum = 0, res = 0;
+    
+    while(r < nums.length){
+        sum += nums[r];
+        while(sum >= target){
+            if(res === 0 || res > r - l + 1) res = r - l + 1;
+            sum -= nums[l];
+            l++;
+        };
+        r++;
+    };
+    return res;
+};
