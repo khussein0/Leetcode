64b5c18a2946b1cb2364965f83a067eeec11514b
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-10-09 12:36:11 +0530

    Time: 116 ms (82.41%), Space: 52 MB (39.59%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    let set = new Set();
+    
+    function dfs(node){
+        if(!node) return false;
+        if(set.has(k - node.val)) return true;
+        set.add(node.val);
+        return dfs(node.left) || dfs(node.right);
+    };
+    
+    return dfs(root);
+};
