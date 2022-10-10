79dbef695b1153ae0023108521a452b6b3b39238
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-10-10 17:27:51 +0530

    Time: 123 ms (59.91%), Space: 46.5 MB (51.40%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let idx = 0, max = 0, target = nums.length - 1;
+    
+    while(idx < nums.length){
+        max = Math.max(max, idx + nums[idx]);
+        
+        if(max >= target) return true;
+        
+        if(max <= idx && nums[idx] === 0) return false;
+        
+        idx++;
+    };
+    
+    return false;
+};
