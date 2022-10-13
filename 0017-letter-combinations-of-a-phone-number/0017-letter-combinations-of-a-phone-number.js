445b40a3c0e77c3b31981f8f8a9757aa86d6c5a1
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-10-13 19:10:54 +0530

    Time: 78 ms (75.20%), Space: 41.9 MB (74.31%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const obj = {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",
+     '6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"};
+    
+    if(!digits.length) return [];
+    let res = [];
+    
+    function combine(str, idx){
+        if(idx === digits.length) res.push(str);
+        else{
+            let letters = obj[digits[idx]];
+            for(let i = 0; i < letters.length; i++){
+                combine(str + letters[i], idx + 1);
+            };
+        };
+    };
+    
+    combine('', 0);
+    return res;
+};
