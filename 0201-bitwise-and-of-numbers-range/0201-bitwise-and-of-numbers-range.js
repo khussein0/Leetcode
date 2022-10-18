a966aa49bd8a21d0068d9828726d41fec6686ca7
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-10-19 02:39:27 +0530

    Time: 311 ms (14.65%), Space: 48.3 MB (92.24%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(left, right) {
+    let i = 0;
+    
+    while (left !== right) {
+        left >>= 1;
+        right >>= 1;
+        i++;
+    };
+	
+    return (left <<= i);
+};
