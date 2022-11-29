7308922f488e53c9ff6b471dcc03af62b8e4d456
khussein0 <49756345+khussein0@users.noreply.github.com>
2022-11-29 12:12:26 +0530

    Time: 396 ms (97.44%), Space: 94.1 MB (84.90%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
@@ -0,0 +1,26 @@
+class RandomizedSet{
+    constructor(){
+        this.map = new Map();
+        this.arr = [];
+    };
+    
+    insert(val){
+        if(this.map.has(val)) return false;
+        this.map.set(val, this.arr.length);
+        this.arr.push(val);
+        return true;
+    };
+    
+    remove(val){
+        if(!this.map.has(val)) return false;
+        this.arr[this.map.get(val)] = this.arr[this.arr.length - 1];
+        this.arr.pop();
+        this.map.set(this.arr[this.map.get(val)], this.map.get(val));
+        this.map.delete(val);
+        return true;
+    };
+    
+    getRandom(){
+        return this.arr[Math.floor(Math.random() * this.arr.length)];
+    };
+}
