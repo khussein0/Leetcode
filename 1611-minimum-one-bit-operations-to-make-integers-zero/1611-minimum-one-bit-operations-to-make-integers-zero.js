3e9477863f66918500a1d0e6ce69e6025726fc50
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-02-07 17:05:47 +0530

    Time: 60 ms (71.43%), Space: 41.7 MB (100.00%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var minimumOneBitOperations = function(n) {
+    let res = 0;
+    while(n > 0){
+        res ^= n;
+        n >>= 1;
+    };
+    return res;
+};
