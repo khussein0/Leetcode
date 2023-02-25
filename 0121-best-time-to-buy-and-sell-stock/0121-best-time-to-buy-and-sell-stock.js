d794de5f49cf3d154771d032d08573d1c90c013e
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-02-25 07:45:45 +0530

    Time: 87 ms (74.55%), Space: 52.2 MB (11.53%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0], res = 0;
+    
+    for(let i = 1; i < prices.length; i++){
+        min = Math.min(min, prices[i]);
+        res = Math.max(res, prices[i] - min);
+    };
+    
+    return res;
+};
