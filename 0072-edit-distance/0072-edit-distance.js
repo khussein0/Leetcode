3bbbb4cf2724835d313f4a37bd2097a420ad048e
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-02-26 10:36:32 +0530

    Time: 92 ms (62.10%), Space: 47.9 MB (29.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -7,11 +7,11 @@ var minDistance = function(word1, word2) {
    let dp = Array(word1.length+1).fill(null).map(()=>(Array(word2.length+1).fill(0)));
 
     for (let i=0;i<dp.length;i++) {
-        dp[i][0] = i
+        dp[i][0] = i;
     };
 
     for (let i=0;i<dp[0].length;i++) {
-        dp[0][i] = i
+        dp[0][i] = i;
     };
 
     for (let i = 1;i<dp.length;i++) {
