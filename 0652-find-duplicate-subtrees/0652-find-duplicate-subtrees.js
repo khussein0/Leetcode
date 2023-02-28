355acbc4a730adf5954c16df48b2f1ce75f8c18a
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-02-28 08:46:13 +0530

    Time: 94 ms (92.37%), Space: 46.7 MB (98.31%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+var findDuplicateSubtrees = function(root) {
+    let map = new Map(), res = [];
+    
+    function dfs(node, map, res){
+        if(!node) return '';
+        let sub = `${node.val}.${dfs(node.left,map,res)}.${dfs(node.right, map,res)}`;
+        map.set(sub, (map.get(sub) || 0) + 1);
+        if(map.get(sub) === 2) res.push(node);
+        return sub;
+    };
+    
+    dfs(root, map, res);
+    return res;
+};
