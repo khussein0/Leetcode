d62ab2d0227c01dd5c7a2159aea0d215af98fa22
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-03-27 12:42:09 +0530

    Time: 74 ms (42.71%), Space: 43.7 MB (62.44%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    let row = grid.length, col = grid[0].length;
+    
+    for(let i = 1; i < row; i++) grid[i][0] += grid[i - 1][0];
+    for(let j = 1; j < col; j++) grid[0][j] += grid[0][j-1];
+    
+    for(let i = 1; i < row; i++){
+        for(let j = 1; j < col; j++) grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
+    };
+    
+    return grid[row - 1][col - 1];
+};
