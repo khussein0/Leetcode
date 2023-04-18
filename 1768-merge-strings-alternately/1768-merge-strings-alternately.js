e40138aca5c69043a666ea319edfdb145a36ddc7
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-04-18 09:42:39 +0530

    Time: 51 ms (92.22%), Space: 41.4 MB (97.41%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let maxLen = Math.max(word1.length, word2.length), res = '';
+    for(let i = 0; i < maxLen; i++) res += (word1[i] ?? '') + (word2[i] ?? '');
+    return res;
+};
