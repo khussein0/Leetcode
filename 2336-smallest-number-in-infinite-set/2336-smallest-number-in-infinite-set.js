968bc1274aecfef49a204dfd4688c16a75a4688a
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-04-25 11:28:48 +0530

    Time: 156 ms (41.51%), Space: 49.9 MB (66.04%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.js
@@ -0,0 +1,31 @@
+
+var SmallestInfiniteSet = function() {
+    this.arr = new Array(1001).fill(true);
+};
+
+/**
+ * @return {number}
+ */
+SmallestInfiniteSet.prototype.popSmallest = function() {
+    for(let i = 1; i < this.arr.length; i++){
+        if(this.arr[i]){
+            this.arr[i] = false;
+            return i;
+        };
+    };
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+SmallestInfiniteSet.prototype.addBack = function(num) {
+    this.arr[num] = true;
+};
+
+/** 
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * var obj = new SmallestInfiniteSet()
+ * var param_1 = obj.popSmallest()
+ * obj.addBack(num)
+ */
