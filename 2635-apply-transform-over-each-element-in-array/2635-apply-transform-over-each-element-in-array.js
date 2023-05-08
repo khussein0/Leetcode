0e04a7124cb412d608e9ac34ca3a2c9c7faf68f4
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-05-08 14:12:59 +0530

    Time: 55 ms (72.79%), Space: 41.8 MB (78.97%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,7 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map(fn);
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++) newArr.push(fn(arr[i], i));
+    return newArr;
 };
