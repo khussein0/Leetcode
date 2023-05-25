3054b2666ad1fe478f175ebd26dffdbbadaaccea
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-05-25 11:58:13 +0530

    Time: 52 ms (95.68%), Space: 44.6 MB (51.68%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    const res = [];
+    while(arr.length > 0) res.push(arr.splice(0, size));
+    return res;
+};
