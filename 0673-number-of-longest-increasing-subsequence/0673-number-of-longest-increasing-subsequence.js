641fc4d041a3f7078a5eed3f271e7e883a5c5789
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-07-21 14:12:07 +0530

    Time: 102 ms (65.38%), Space: 43.9 MB (73.08%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.js
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var findNumberOfLIS = function(nums) {
-    const dp = Array(nums.length).fill(1);
-    const paths = Array(nums.length).fill(1);
+    const dp = Array(nums.length).fill(1), paths = Array(nums.length).fill(1);
     
     for(let i = 0; i < nums.length; i++) {
         for(let j = i-1; j >= 0; j--) {
@@ -15,11 +14,13 @@ var findNumberOfLIS = function(nums) {
             };
         };
     };
+    
     const max = Math.max(...dp);
     let totalPaths = 0;
     
     for(let i = 0; i < dp.length; i++) {
         if(dp[i] === max) totalPaths += paths[i];
     };
+    
     return totalPaths;
 };
