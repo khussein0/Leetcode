d3a00a38bf6b82d277c78d143547951f11728086
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-07-25 16:51:45 +0530

    Time: 79 ms (37.04%), Space: 50.6 MB (97.53%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -2,6 +2,11 @@
  * @param {number[]} arr
  * @return {number}
  */
-var peakIndexInMountainArray = function(arr) {
-    return arr.indexOf(Math.max(...arr));
+var peakIndexInMountainArray = function(arr, l = 0, r = arr.length - 1) {
+    if(!arr.length) return -1;
+    const mid = ((l + r) >> 1) | 0;
+    
+    if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+    if(arr[mid] < arr[mid - 1]) return peakIndexInMountainArray(arr, l, mid);
+    if(arr[mid] < arr[mid + 1]) return peakIndexInMountainArray(arr, mid, r);
 };
