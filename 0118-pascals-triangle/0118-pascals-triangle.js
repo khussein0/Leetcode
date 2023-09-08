b4f37550d85380820b87c0e367a1dbdb75a7c2a8
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-09-08 11:31:59 +0530

    Time: 51 ms (74.70%), Space: 41.9 MB (84.59%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const res = [];
+    
+    for(let i = 0; i < numRows; i++){
+        res.push(Array(i+1));
+        for(let j = 0; j <= i; j++){
+            if(j === 0 || j === i) res[i][j] = 1;
+            else res[i][j] = res[i-1][j-1] + res[i-1][j];
+        };
+    };
+    
+    return res;
+};
