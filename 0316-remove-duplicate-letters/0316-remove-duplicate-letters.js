552fc24253296b876916e0aa9d04cdfe42795091
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-09-26 22:03:50 +0530

    Time: 64 ms (44.59%), Space: 42.4 MB (66.22%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function(s) {
+    const stack = [], seen = new Set(), last = {};
+    
+    for(let i = 0; i < s.length; i++) last[s[i]] = i;
+    
+    for(let i = 0; i < s.length; i++){
+        let cur = s[i];
+        if(!seen.has(cur)){
+            while(stack.length && cur < stack[stack.length - 1] && i < last[stack[stack.length - 1]]) seen.delete(stack.pop());
+            seen.add(cur);
+            stack.push(cur);
+        };
+    };
+    
+    return stack.join('');
+};
