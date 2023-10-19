75f04bde7bbc8e35b0e65a1ea7baf64cf66f7c8b
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-10-19 10:36:50 +0530

    Time: 51 ms (72.62%), Space: 43.6 MB (22.13%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    function backspace(str){
+        let res = [];
+        for(const i of str.split('')) i === "#" ? res.pop() : res.push(i);
+        return res.join('');
+    };
+    
+    return backspace(s) === backspace(t);
+};
