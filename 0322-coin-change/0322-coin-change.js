f5c415f773e99180fb417a5f3dd74e5737e79f68
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-10-23 15:40:53 +0530

    Time: 86 ms (76.72%), Space: 45.3 MB (95.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -4,12 +4,14 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-  const dp = Array(amount + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    const dp = new Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    
+    for(const coin of coins){
+        for(let i = coin; i <= amount; i++){
+            dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+        };
     };
-  };
-  return dp[amount] === Infinity ? -1 : dp[amount];
+    
+    return dp[amount] === Infinity ? -1 : dp[amount];
 };
