e3e74519854dac16e03cb642836c512b00b4102b
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-10-30 17:01:18 +0530

    Time: 136 ms (15.74%), Space: 48.5 MB (23.15%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function(arr) {
+    return arr.sort((a,b) => {
+        let countA = a.toString(2).split('').filter(bit => bit === '1').length;
+        let countB = b.toString(2).split('').filter(bit => bit === '1').length;
+        return countA - countB || a - b;
+    });
+};
