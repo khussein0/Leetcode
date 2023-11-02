e288addbd53ae38b09e4329102e2a58899a78aaa
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-11-02 19:12:37 +0530

    Time: 54 ms, Memory: 43.9 MB - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) return '';
+    
+    for(let i = 0; i < strs[0].length; i++){
+        for(let s of strs){
+            if(s[i] !== strs[0][i]) return s.slice(0, i);
+        };
+    };
+    
+    return strs[0];
+};
