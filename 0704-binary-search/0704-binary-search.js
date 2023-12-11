6b43cf58965700fd05298dbc3b134e9fd2068840
khussein0 <49756345+khussein0@users.noreply.github.com>
2023-12-11 14:02:24 +0530

    Time: 52 ms (76.37%), Space: 44.3 MB (95.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -7,7 +7,7 @@ var search = function(nums, target) {
     let l = 0, r = nums.length - 1;
     
     while(l <= r){
-        let mid = (l + r) >> 1;
+        let mid = (l+r) >> 1;
         if(nums[mid] === target) return mid;
         else if(nums[mid] > target) r = mid - 1;
         else l = mid + 1;
