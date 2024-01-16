747b86ba26071e0bed8677bfd86ac6b36f6a6121
khussein0 <49756345+khussein0@users.noreply.github.com>
2024-01-16 11:12:09 +0530

    Time: 326 ms (66.76%), Space: 111.2 MB (57.86%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
@@ -1,26 +1,43 @@
-class RandomizedSet{
-    constructor(){
-        this.map = new Map();
-        this.arr = [];
-    };
-    
-    insert(val){
-        if(this.map.has(val)) return false;
-        this.map.set(val, this.arr.length);
-        this.arr.push(val);
-        return true;
-    };
-    
-    remove(val){
-        if(!this.map.has(val)) return false;
-        this.arr[this.map.get(val)] = this.arr[this.arr.length - 1];
-        this.arr.pop();
-        this.map.set(this.arr[this.map.get(val)], this.map.get(val));
-        this.map.delete(val);
-        return true;
-    };
-    
-    getRandom(){
-        return this.arr[Math.floor(Math.random() * this.arr.length)];
-    };
-}
+
+var RandomizedSet = function() {
+    this.map = new Map();
+    this.arr = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if(this.map.has(val)) return false;
+    this.map.set(val, this.arr.length);
+    this.arr.push(val);
+    return true;
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if(!this.map.has(val)) return false;
+    this.arr[this.map.get(val)] = this.arr[this.arr.length - 1];
+    this.arr.pop();
+    this.map.set(this.arr[this.map.get(val)], this.map.get(val));
+    this.map.delete(val);
+    return true;
+};
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    return this.arr[Math.floor(Math.random() * this.arr.length)];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
