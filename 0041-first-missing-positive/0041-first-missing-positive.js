3613ce09fcec781aabde8da0379461e7abb16df7
khussein0 <49756345+khussein0@users.noreply.github.com>
2024-03-26 10:42:12 +0530

    Time: 63 ms (91.59%), Space: 57.5 MB (67.39%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        let idx = nums[i] - 1;
+        if(i === idx || nums[i] === nums[idx]) continue;
+        if(idx >= 0 && idx <= nums.length - 1){
+            [nums[i], nums[idx]] = [nums[idx], nums[i]];
+            i--;
+        }
+    }
+    
+    for(let i = 0; i < nums.length; i++){
+        if(i+1 !== nums[i]) return i+1; 
+    }
+    
+    return nums.length + 1;
+};
